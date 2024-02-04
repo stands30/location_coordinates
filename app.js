a4a8aa97119dc767b26da607eeff8243d3b157ac
Stanley Dsouza <lancylotdsouza@gmail.com>
2024-02-04 20:26:35 +0530

    app updated

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ mongoose
 
 /** database connection ends */
 if(  process.env.NODE_ENV != 'production'){
+  console.log('development mode');
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
